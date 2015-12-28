b122060862e7794ca97e24920f0c3d86a3816203
Satori <shakeebmajid@ufl.edu>
2015-12-28 15:18:12 -0500

    Initial Commit

---
+++ TipJar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipJar
+//
+//  Created by Shakeeb Majid on 12/28/15.
+//  Copyright (c) 2015 Shakeeb Majid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
