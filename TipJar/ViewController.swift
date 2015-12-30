7f8b1ceb6389262c650fd0f07d7082d0e1b35d88
Satori <shakeebmajid@ufl.edu>
2015-12-30 02:56:44 -0500

    Added default tip percentages

--- TipJar/ViewController.swift
+++ TipJar/ViewController.swift
@@ -8,16 +8,129 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipValue = defaults.doubleForKey("default_tip_percentage")
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipValue
+        var total = billAmount + tip
+        
+        
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+        if(tipValue == 0.18){
+            tipControl.selectedSegmentIndex = 0
+        }
+        
+        if(tipValue == 0.20 ){
+            tipControl.selectedSegmentIndex = 1
+            
+        }
+        
+        if(tipValue == 0.22){
+            tipControl.selectedSegmentIndex = 2
+            
+        }
+        
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipValue = defaults.doubleForKey("default_tip_percentage")
+        
+        let changeValue = defaults.boolForKey("changed")
+        
+        if(changeValue){
+            var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+            var tip = billAmount * tipValue
+            var total = billAmount + tip
+            
+            
+            
+            tipLabel.text = "$\(tip)"
+            totalLabel.text = "$\(total)"
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+            
+            
+            if(tipValue == 0.18){
+                tipControl.selectedSegmentIndex = 0
+            }
+            
+            if(tipValue == 0.20 ){
+                tipControl.selectedSegmentIndex = 1
+                
+            }
+            
+            if(tipValue == 0.22){
+                tipControl.selectedSegmentIndex = 2
+                
+            }
+
+            
+        }
+        
+
+    }
+    
+    
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.20, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
     }
 
 
